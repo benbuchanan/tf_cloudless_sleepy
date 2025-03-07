1e40f526b82ac476047800121eab32030a93d9b1
Ben Buchanan <buchanan@ibm.com>
2025-03-07 14:32:41 -0500

    sleepy fix

--- vars.tf
+++ vars.tf
@@ -1,11 +1,5 @@
-# Configure these variables
-
-variable "sample_var" {
-  description = "A sample_var to pass to the template."
-  default     = "hello"
-}
-
-variable "sleepy_time" {
-  description = "How long our local-exec will take a nap."
-  default     = 0
+variable "sleep_duration" {
+  description = "The amount of time to sleep (in seconds)"
+  type        = number
+  default     = 10
 }
